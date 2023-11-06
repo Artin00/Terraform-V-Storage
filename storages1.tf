7f92d71265d5325bccde6694cb34c67a464bcfe5
Artin00 <113461257+Artin00@users.noreply.github.com>
2023-11-06 14:48:17 +0000

    Added network_rules
    This contains the network_rules to make the network private and only accept two subnets

---
+++ storages1.tf
@@ -0,0 +1,43 @@
+data "azurerm_resource_group" "DataCohortLab" { # Store the resource group that has already been created within Azure
+  name = "rg-data-cohort-labs"
+}
+
+resource "azurerm_storage_account" "Storaccount1" { # The storage account that contains a delete_retention_policy
+  name                     = "akstrgccntqualyfi"
+  resource_group_name      = data.azurerm_resource_group.DataCohortLab.name
+  location                 = data.azurerm_resource_group.DataCohortLab.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  blob_properties{
+    delete_retention_policy{
+      days = 30
+    }
+  }
+}
+
+resource "azurerm_storage_account_network_rules" "NetWorkRules1" { # This resource creates the network policy for the storage account created above
+  storage_account_id         = azurerm_storage_account.Storaccount1.id
+  default_action             = "Deny"
+  virtual_network_subnet_ids = [azurerm_subnet.Subnet1.id,azurerm_subnet.Subnet2.id]
+}
+
+resource "time_sleep" "wait_for_creation" {
+  depends_on = [azurerm_storage_account.Storaccount1]
+  create_duration   = "30s"
+}
+
+resource "azurerm_storage_container" "Bronze1" { # Creating the Bronze container within the storage account akstrgccntqualyfi
+  name                 = "bronze"
+  storage_account_name = azurerm_storage_account.Storaccount1.name
+}
+
+resource "azurerm_storage_container" "Silver1" { # Creating the Silver container within the storage account akstrgccntqualyfi
+  name                 = "silver"
+  storage_account_name = azurerm_storage_account.Storaccount1.name
+}
+
+resource "azurerm_storage_container" "Gold1" { # Creating the Gold container within the storage account akstrgccntqualyfi
+  name                 = "gold"
+  storage_account_name = azurerm_storage_account.Storaccount1.name
+}
